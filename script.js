17ef2b7a820dad57fab659a90a0f15fc12956efe
yogiankit45 <YOGIANKIT45@GMAIL.COM>
2025-02-26 12:31:20 +0530

    yogipdf

---
+++ script.js
@@ -0,0 +1,11 @@
+// Dummy function to simulate merging PDFs
+function mergePDF() {
+    alert("Merging PDFs...");
+    // You can add actual functionality using PDF libraries like PDF.js or others.
+}
+
+// Dummy function to simulate compressing PDFs
+function compressPDF() {
+    alert("Compressing PDF...");
+    // You can add actual functionality here as well.
+}
